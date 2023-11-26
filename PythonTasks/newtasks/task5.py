9e4cedec3dbf8e609c476c3d5134e82bf8a5097d
burzeppe <kazancev789@mail.ru>
2023-11-27 02:15:38 +0300

    дз

---
+++ PythonTasks/newtasks/task5.py
@@ -0,0 +1,7 @@
+summa = 0
+dictation = dict(Artyom = '15', Danil = '18')
+dictation.update({'Kirill': '17'})
+for key, value in dictation.items():
+    summa += int(value)
+print('Сумма возрастов равна', summa)
+#ялквмом
